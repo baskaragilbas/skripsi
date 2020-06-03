6aa36b561142e354c2bcd649d8182addd26d15c3
aftermath089 <vincentmichael089@gmail.com>
2020-06-03 16:11:10 +0800

    remove ReportListItem, handle form-select in ReportList

--- src/components/ReportList.vue
+++ src/components/ReportList.vue
@@ -1,31 +1,55 @@
 <template>
   <div>
-    <h3>Daftar Report</h3>
+    <div class="text-left">Pilih rute</div>
     <b-form>
       <div>
-      <b-table-simple hover small caption-top responsive>
-        <ReportListItem
-          v-for="route in routes"
-          v-bind:key="route.routeName"
-          v-bind:route="route"/>
-      </b-table-simple>
-     </div>
-    </b-form> 
-
+         <b-form-select 
+          v-model="selected" 
+          v-bind:options="options"
+          v-on:change="getSelected"></b-form-select>
+      </div>
+    </b-form>
+    <br/>
   </div>
 </template>
 
 <script>
-import ReportListItem from '@/components/ReportListItem.vue'
-
 export default {
-  components: {
-    ReportListItem
-  },
   props: {
     routes: {
       required: true,
       type: Array
+    }
+  },
+  data () {
+    return {
+      selected: null,
+    }
+  },
+
+  computed: {
+    options () {
+      return this.routes.map((item) => {
+        const value = item.id
+        const text = item.routeName
+
+        return {
+          value: value, 
+          text: text
+        }
+      })
+    }
+  },
+
+  methods: {
+    getSelected(args){
+      const selectedRoute = {...this.routes.filter(
+        route => {
+          return route.id === args
+        }
+      )}
+
+      this.$router.push({name: 'ReportForm', params: {id: selectedRoute[0].routeName, route: selectedRoute[0]}})
     }
   }
 }
