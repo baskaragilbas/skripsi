ff2ef67ddb900c1df046a296976a0b6d7bfe42c1
baskaragilbas <baskaragilbas@gmail.com>
2020-06-12 14:45:20 +0700

    adding extrafiles in electron-builder option

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,26 @@
 module.exports = {
   pluginOptions: {
     electronBuilder: {
-      externals: ['sequelize']
+      externals: ['sequelize'],
+      builderOptions: {
+        // options placed here will be merged with default configuration and passed to electron-builder
+        files: [
+          "**/*"
+        ],
+        extraFiles: [
+          {
+            "from": "models",
+            "to": "models",
+            "filter": ["**/*"]
+          },
+          {
+              "from": "src/db",
+              "to": "src/db",
+              "filter": ["**/*"]
+          }
+        ]
+      }
+    
     }
   }
   }
